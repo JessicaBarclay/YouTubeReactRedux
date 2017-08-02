64e1cb8b73e0d4f0d63ca12343b3a5cf4e405cd3
Barclay <BARCLAJX@legal.regn.net>
2017-08-02 12:23:14 +0100

    Add static SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCr9KG9a3t_JAQBlSeqoPnSNXdMBYd3FLY';
 
 const App = () => {
-    return <div>Test Page</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
