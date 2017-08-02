5213ec098bc34d828d253b106b743b2803766fc5
Barclay <BARCLAJX@legal.regn.net>
2017-08-02 12:18:16 +0100

    Add YouTube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCr9KG9a3t_JAQBlSeqoPnSNXdMBYd3FLY';
+
 const App = () => {
     return <div>Test Page</div>;
 }
