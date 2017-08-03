62b28bbe1c7f7f5267c3e0eef7a8a6ce77fd940c
Barclay <BARCLAJX@legal.regn.net>
2017-08-03 09:08:24 +0100

    Rename of state.term to state.input for clarity

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+    constructor(props) {
+        super(props);
+
+        this.state = { input: '' };
     }
+
+    render() {
+        return ( 
+            <div>
+                <input onChange={event => this.setState({ input: event.target.value })}/>
+                Value of input: {this.state.input}
+            </div>
+        );
+        }
 }
 
 export default SearchBar;
