c284629b57b6b466d8ef93115f59065ae7f0f3e5
Barclay <BARCLAJX@legal.regn.net>
2017-08-09 13:46:11 +0100

    Added onVideoSelect function, passed from parent to child to child. User can click on a video from list to play it

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
 // video above, is equal to declaring a new variable equal to props.video
 // from videolist
   const imageURL = video.snippet.thumbnails.default.url
   const videoHeading = video.snippet.title;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={ imageURL } />
         </div>
         <div className="media-body">
           <div className="media-heading">
-          { videoHeading }
+            {videoHeading}
           </div>
         </div>
       </div>
