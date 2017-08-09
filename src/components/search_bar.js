342f0b1ec146b62219c25ae5600696cd482ffca1
Barclay <BARCLAJX@legal.regn.net>
2017-08-09 14:08:06 +0100

    User can search for videos using term in search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,19 +4,22 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { input: '' };
+    this.state = {term: ''};
   }
 
   render() {
     return (
       <div className="search-bar">
         <input
-        value={this.state.input}
-        onChange={event => this.setState({ input: event.target.value })}
-        />
-        {this.state.input}
+        value={this.state.term}
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
